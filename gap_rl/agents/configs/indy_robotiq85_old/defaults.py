3ea0c68dba548e6f9003dc9d377ff6052a5b80d8
bklee <byungkun.lee@kaist.ac.kr>
2025-05-07 00:35:02 +0000

    asdfs

---
+++ gap_rl/agents/configs/indy_robotiq85_old/defaults.py
@@ -0,0 +1,137 @@
+from gap_rl import DESCRIPTION_DIR
+from gap_rl.agents.controllers import *
+from gap_rl.sensors.camera import CameraConfig
+from gap_rl.sensors.depth_camera import StereoDepthCameraConfig
+
+
+class IndyRobotiq85oldDefaultConfig:
+    def __init__(self) -> None:
+        self.urdf_path = f"{DESCRIPTION_DIR}/ur5e/indy_robotiq85_old.urdf"
+        self.urdf_config = {}
+
+        self.arm_joint_names = [
+            "shoulder_pan_joint",
+            "shoulder_lift_joint",
+            "elbow_joint",
+            "wrist_1_joint",
+            "wrist_2_joint",
+            "wrist_3_joint",
+        ]
+        self.arm_stiffness = 1000  # 1e3
+        self.arm_damping = 50  # 1e2
+        self.arm_force_limit = 100
+        self.arm_delta = 0.04  # 0.05 rad/(control step)
+        self.arm_vel_delta = 0.2  # 0.2 rad/s
+
+        self.gripper_joint_names = [
+            "robotiq_2f_85_left_driver_joint",
+            "robotiq_2f_85_right_driver_joint",
+        ]
+        self.gripper_stiffness = 1e3  # 1e3
+        self.gripper_damping = 3e2  # 1e2
+        self.gripper_force_limit = 100
+
+        self.ee_link_name = "grasp_convenient_link"
+        self.ee_delta = 0.01
+        self.rot_bound = 0.1
+        self.rot_euler_bound = 0.05  # ~2.86deg
+
+    @property
+    def controllers(self):
+        # -------------------------------------------------------------------------- #
+        # Arm
+        # -------------------------------------------------------------------------- #
+        # PD ee position
+        arm_pd_ee_delta_pos = PDEEPosControllerConfig(
+            self.arm_joint_names,
+            -self.ee_delta,
+            self.ee_delta,
+            stiffness=self.arm_stiffness,
+            damping=self.arm_damping,
+            force_limit=self.arm_force_limit,
+            ee_link=self.ee_link_name,
+        )
+        arm_pd_ee_delta_pose = PDEEPoseControllerConfig(
+            self.arm_joint_names,
+            -self.ee_delta,
+            self.ee_delta,
+            self.rot_bound,
+            stiffness=self.arm_stiffness,
+            damping=self.arm_damping,
+            force_limit=self.arm_force_limit,
+            ee_link=self.ee_link_name,
+        )
+        arm_pd_ee_delta_pose_euler = PDEEPoseEulerControllerConfig(
+            self.arm_joint_names,
+            -self.ee_delta,
+            self.ee_delta,
+            self.rot_euler_bound,
+            stiffness=self.arm_stiffness,
+            damping=self.arm_damping,
+            force_limit=self.arm_force_limit,
+            cache_size=3,  # smooth action cache size
+            frame="ee",
+            smooth=False,  # smooth action or not
+            ee_link=self.ee_link_name,
+            normalize_action=False,
+        )
+
+        # -------------------------------------------------------------------------- #
+        # Gripper
+        # -------------------------------------------------------------------------- #
+        gripper_pd_joint_pos = PDJointPosMimicControllerConfig(
+            self.gripper_joint_names,
+            0,
+            0.0425,
+            stiffness=self.gripper_stiffness,
+            damping=self.gripper_damping,
+            force_limit=self.gripper_force_limit,
+            friction=0.2,
+            # interpolate=True,
+            normalize_action=False,
+        )
+
+        controller_configs = dict(
+            pd_ee_delta_pos=dict(arm=arm_pd_ee_delta_pos, gripper=gripper_pd_joint_pos),
+            pd_ee_delta_pose=dict(
+                arm=arm_pd_ee_delta_pose, gripper=gripper_pd_joint_pos
+            ),
+            pd_ee_delta_pose_euler=dict(
+                arm=arm_pd_ee_delta_pose_euler, gripper=gripper_pd_joint_pos
+            )
+        )
+
+        # Make a deepcopy in case users modify any config
+        return deepcopy_dict(controller_configs)
+
+    @property
+    def cameras(self):
+        return [
+            CameraConfig(
+                uid="hand_realsense",
+                p=[0.0, 0.0, 0.0],
+                q=[1, 0, 0, 0],
+                width=320,
+                height=180,
+                fov=0.758,
+                near=0.01,
+                far=5,
+                actor_uid="camera_hand_link",
+                hide_link=False,
+            ),  # sapien camera config
+            # StereoDepthCameraConfig(
+            #     uid="hand_stereo",
+            #     p=[0.0, 0.0, 0.0],
+            #     q=[1, 0, 0, 0],
+            #     width=320,
+            #     height=180,
+            #     ir_width=848,  # 848
+            #     ir_height=480,  # 480
+            #     min_depth=0.01,
+            #     fov=0.758,  # 0.758, 1.137
+            #     near=0.01,
+            #     far=5,
+            #     actor_uid="camera_hand_link",
+            #     hide_link=False,
+            # )
+        ]
---
+++ gap_rl/agents/robots/indy_robotiq85_old.py
@@ -0,0 +1,134 @@
+from pathlib import Path
+
+import numpy as np
+import sapien.core as sapien
+from gap_rl import DESCRIPTION_DIR
+from gap_rl.agents.base_agent import BaseAgent, parse_urdf_config
+from gap_rl.agents.configs.ur5e_robotiq85_old import defaults
+from gap_rl.utils.common import compute_angle_between
+from gap_rl.utils.geometry import transform_points
+from gap_rl.utils.sapien_utils import (
+    get_entity_by_name,
+    get_pairwise_contact_impulse,
+    get_multi_pairwise_contact_impulse,
+)
+from gap_rl.utils.trimesh_utils import get_actor_mesh
+
+
+class UR5e_Robotiq85_old(BaseAgent):
+    _config: defaults.UR5eRobotiq85oldDefaultConfig
+
+    @classmethod
+    def get_default_config(cls):
+        return defaults.UR5eRobotiq85oldDefaultConfig()
+
+    def _after_init(self):
+        self.finger1_link: sapien.LinkBase = get_entity_by_name(
+            self.robot.get_links(), "left_inner_finger_pad"
+        )
+        self.finger2_link: sapien.LinkBase = get_entity_by_name(
+            self.robot.get_links(), "right_inner_finger_pad"
+        )
+        self.finger1_mesh = get_actor_mesh(self.finger1_link, False)
+        self.finger2_mesh = get_actor_mesh(self.finger2_link, False)
+        self.finger_size = (0.038, 0.008, 0.02)  # values from URDF
+
+    def check_grasp(self, actor: sapien.ActorBase, min_impulse=1e-6, max_angle=85):
+        assert isinstance(actor, sapien.ActorBase), type(actor)
+        contacts = self.scene.get_contacts()
+
+        # finger와 object간의 contact
+        limpulse = get_pairwise_contact_impulse(contacts, self.finger1_link, actor)
+        rimpulse = get_pairwise_contact_impulse(contacts, self.finger2_link, actor)
+
+        # direction to open the gripper
+        ldirection = -self.finger1_link.pose.to_transformation_matrix()[:3, 1]
+        rdirection = -self.finger2_link.pose.to_transformation_matrix()[:3, 1]
+
+        # angle between impulse and open direction
+        langle = compute_angle_between(ldirection, limpulse)
+        rangle = compute_angle_between(rdirection, rimpulse)
+
+        lflag = (
+            np.linalg.norm(limpulse) >= min_impulse and np.rad2deg(langle) <= max_angle
+        )
+        rflag = (
+            np.linalg.norm(rimpulse) >= min_impulse and np.rad2deg(rangle) <= max_angle
+        )
+
+        return all([lflag, rflag])
+
+    def check_contact_fingers(self, actor: sapien.ActorBase, min_impulse=1e-6):
+        assert isinstance(actor, sapien.ActorBase), type(actor)
+        contacts = self.scene.get_contacts()
+
+        limpulse = get_pairwise_contact_impulse(contacts, self.finger1_link, actor)
+        rimpulse = get_pairwise_contact_impulse(contacts, self.finger2_link, actor)
+
+        return (
+            np.linalg.norm(limpulse) >= min_impulse,
+            np.linalg.norm(rimpulse) >= min_impulse,
+        )
+
+    def check_contact(self, actor: sapien.ActorBase, min_impulse=1e-6):
+        contacts = self.scene.get_contacts()
+        # obj_contact_actors = [contact.actor1 for contact in contacts if contact.actor0 == actor] \
+        #                      + [contact.actor0 for contact in contacts if contact.actor1 == actor]
+        # contact_actors = [c_actor for c_actor in obj_contact_actors if c_actor in self.robot_collision_actors]
+        multi_impluse = np.linalg.norm(
+            get_multi_pairwise_contact_impulse(contacts, self.robot_collision_actors, actor), axis=-1
+        )
+        is_agent_contact = any(multi_impluse >= min_impulse)
+        return is_agent_contact, multi_impluse
+
+    @staticmethod
+    def build_grasp_pose(approaching, closing, center):
+        assert np.abs(1 - np.linalg.norm(approaching)) < 1e-3
+        assert np.abs(1 - np.linalg.norm(closing)) < 1e-3
+        assert np.abs(approaching @ closing) <= 1e-3
+        ortho = np.cross(approaching, closing)
+        T = np.eye(4)
+        T[:3, :3] = np.stack([approaching, closing, ortho], axis=1)
+        T[:3, 3] = center
+        return sapien.Pose.from_transformation_matrix(T)
+
+    def sample_ee_coords(self, num_sample=10) -> np.ndarray:
+        """Uniformly sample points on the two finger meshes. Used for dense reward computation
+        return: ee_coords (2, num_sample, 3)"""
+        finger1_points = transform_points(
+            self.finger1_link.get_pose().to_transformation_matrix(), self.finger1_mesh.sample(num_sample)
+        )
+        finger2_points = transform_points(
+            self.finger2_link.get_pose().to_transformation_matrix(), self.finger1_mesh.sample(num_sample)
+        )
+        ee_coords = np.stack((finger1_points, finger2_points))
+
+        return ee_coords
+
+    def _load_articulation(self):
+        loader = self.scene.create_urdf_loader()
+
+        urdf_path = str(self.urdf_path)
+        urdf_path = urdf_path.format(description=DESCRIPTION_DIR)
+        urdf_config = parse_urdf_config(self.urdf_config, self.scene)
+
+        builder = loader.load_file_as_articulation_builder(urdf_path, urdf_config)
+
+        # Disable self collision for simplification
+        for link_builder in builder.get_link_builders():
+            link_builder.set_collision_groups(1, 1, 2, 0)
+        self.robot = builder.build(fix_root_link=self.fix_root_link)
+        assert self.robot is not None, f"Fail to load URDF from {urdf_path}"
+        self.robot.set_name(Path(urdf_path).stem)
+        # Cache robot link ids
+        self.robot_link_ids = [link.get_id() for link in self.robot.get_links()]
+        self.robot_collision_actors = [actor for actor in self.robot.get_links() if not actor.get_collision_shapes() == []]
+        active_joint_name = [joint.get_name() for joint in self.robot.get_active_joints()]
+        self.gripper_joint_ids = [active_joint_name.index(joint_name) for joint_name in self.config.gripper_joint_names]
+        self.agent_link_names = ['base_link', 'shoulder_link', 'upper_arm_link', 'forearm_link', 'wrist_1_link', 'wrist_2_link', 'wrist_3_link', 'ee_link']
+        self.handcam_link_names = ['camera_hand_color_frame', 'camera_hand_link', 'camera_hand_color_optical_frame', 'camera_hand_depth_optical_frame']
+        self.gripper_link_names = [
+            'robotiq_arg2f_base_link', 'robotiq_85_left_knuckle_link',  'robotiq_85_left_inner_knuckle_link', 'robotiq_85_left_finger_link',
+            'robotiq_85_right_knuckle_link', 'robotiq_85_right_inner_knuckle_link', 'robotiq_85_right_finger_link'
+        ]
+        self.gripper_pad_names = ['robotiq_85_left_finger_tip_link', 'robotiq_85_right_finger_tip_link']
